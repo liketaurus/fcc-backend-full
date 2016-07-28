2a5f82a7fdf3c88541a0da663c82891c83543286
Alexander Babich <Alexander.taurus@gmail.com>
2016-07-28 14:48:35 +0000

    package.json fixed

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var moment = require('moment');
 var app = express();
-var port = process.env.PORT;
+var port = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({
     extended: true
